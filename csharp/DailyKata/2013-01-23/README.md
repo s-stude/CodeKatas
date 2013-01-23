bd9ad49a1fa528017025629c077ca47246242dc0
Akim Khalilov <Akim.Khalilov@gmail.com>
2013-01-23 11:36:19 +0200

    Add structure again

---
+++ csharp/DailyKata/2013-01-23/README.md
@@ -0,0 +1,2 @@
+# First kata goes here
+
---
+++ csharp/src/TDDKata/Kata.Core/StringCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Kata.Core
+{
+	public class StringCalculator
+	{
+		public int Add(string numbers)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
